6283fad19ebdaa120a6f8f40c75b06cef46e8d71
Iyanu <akinyemiiyanu45@gmail.com>
2022-08-20 13:37:23 +0100

    iyanu commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UserpageComponent } from './userpage/userpage.component';
+import { UsersignupComponent } from './usersignup/usersignup.component';
+
+const routes: Routes = [
+
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'signup', component: UsersignupComponent },
+    {path: 'login', component: UserloginComponent},
+    {path: 'userpage', component: UserpageComponent},
+    {path: 'course', component: CourseComponent},
+    {path: 'about', component: AboutComponent},
+
+    {path: '**', component: NotfoundComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

7a791d567ea21a6c2f888a6f2e67fbed0506fbf8
Paul Oluwaseyi <paulisola784@gmail.com>
2022-08-29 17:41:38 +0100

    I just committed to switch to main branch and confirm some stuffs

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-course',
@@ -7,11 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCourseComponent implements OnInit {
 
-  constructor() { }
+  public addCourse: FormGroup;
+
+  public courseCategory = "";
+
+  constructor(
+    private _fb: FormBuilder
+  ) { 
+    this.addCourse = this._fb.group({
+      courseName: ['', Validators.required],
+      courseCategory: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
+    // console.log(this.addCourse.controls.courseName.value);
+    console.log("Hello world")
   }
   public favoriteSeason = "";
   seasons: string[] = ['Free', 'Paid'];
 
+  saveCourse() {
+    console.log("Hello world")
+    let courseName = this.addCourse.controls.courseName.value;
+    // let courseCategory = this.addCourse.controls.courseCategory.value
+
+    let courseObj = {
+      courseName,
+    }
+
+    console.log(courseObj);
+  }
 }
